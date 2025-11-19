1527af5fb0accb9561fa4a526176fab6ebf98552
Mario Medina <mariomedinam@gmail.com>
2025-11-19 13:12:32 -0500

    Update button styles in CartasFianza component for improved UI consistency
    - Modified button classes in the `CartasFianza` component to enhance visual consistency, adding borders and adjusting background colors for better user experience.
    - Ensured that buttons maintain a cohesive design across different sections of the component.

--- frontend/src/pages/CartasFianza.js
+++ frontend/src/pages/CartasFianza.js
@@ -377,7 +377,7 @@ const CartasFianza = () => {
                                                         {/* Botón Ver detalle */}
                                                         <button
                                                           onClick={() => handleVerDetalle(history.id)}
-                                                          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-primary-700 bg-primary-50 rounded-lg hover:bg-primary-100 focus:ring-2 focus:ring-primary-300 whitespace-nowrap"
+                                                          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-primary-700  rounded-lg hover:bg-primary-100 focus:ring-2 focus:ring-primary-300 whitespace-nowrap border border-primary-500"
                                                         >
                                                           <svg className="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
@@ -416,7 +416,7 @@ const CartasFianza = () => {
                                                         {/* Botón Ver detalle */}
                                                         <button
                                                           onClick={() => handleVerDetalle(history.id)}
-                                                          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-lg hover:bg-gray-50 focus:ring-2 focus:ring-gray-300 whitespace-nowrap"
+                                                          className="inline-flex items-center justify-center px-4 py-2 text-sm font-medium text-primary-700  rounded-lg hover:bg-primary-100 focus:ring-2 focus:ring-primary-300 whitespace-nowrap border border-primary-500"
                                                         >
                                                           <svg className="w-4 h-4 mr-1.5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
